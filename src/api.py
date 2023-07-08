ad731c49f2cb74f45f191e22c86c9ab4e00acf10
Alexandra <alexandrasolovjova@mail.ru>
2023-07-08 23:36:53 +0300

    update get_info

--- src/api.py
+++ src/api.py
@@ -1,16 +1,22 @@
+import requests
+import json
 from abc import ABC, abstractmethod
+
 
 class Api(ABC):
     @abstractmethod
     def get_info(self, key):
         pass
 
+
 class HeadHunter(Api):
     def __init__(self):
         pass
 
     def get_info(self, key):
-        pass
+        params = {"area": 113, "text": key, "per_page": 10}
+        response = requests.get('https://api.hh.ru/vacancies', params)
+        print(*response.json()['items'], sep='\n')
 
 
 class SuperJob(Api):
@@ -19,3 +25,7 @@ class SuperJob(Api):
 
     def get_info(self, key):
         pass
+
+
+hh = HeadHunter()
+hh.get_info('Python-разработчик')
