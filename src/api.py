c6fe61ffa07ca4b102445fa9e2ee78038eaff400
Alexandra <alexandrasolovjova@mail.ru>
2023-07-10 18:22:11 +0300

    add create_info method in HeadHunter

--- src/api.py
+++ src/api.py
@@ -16,7 +16,39 @@ class HeadHunter(Api):
     def get_info(self, key):
         params = {"area": 113, "text": key, "per_page": 10}
         response = requests.get('https://api.hh.ru/vacancies', params)
-        print(*response.json()['items'], sep='\n')
+        return response.json()['items']
+
+    def output_info(self, key):
+        hh_output = self.get_info(key)
+        output = []
+        for info in hh_output:
+            id = info['id']
+            name = info['name']
+            link = info['alternate_url']
+            if info['salary'] is not None:
+                salary = {
+                    'from': info['salary']['from'],
+                    'to': info['salary']['to']
+                }
+            else:
+                salary = None
+            if info['snippet'] is not None:
+                snippet = []
+                for key, value in info['snippet'].items():
+                    if value is not None:
+                        snippet.append(value)
+                description = ' '.join(snippet)
+            else:
+                description = None
+            item = {
+                'id': id,
+                'name': name,
+                'link': link,
+                'salary': salary,
+                'description': description
+            }
+            output.append(item)
+        return output
 
 
 class SuperJob(Api):
@@ -28,4 +60,5 @@ class SuperJob(Api):
 
 
 hh = HeadHunter()
-hh.get_info('Python-разработчик')
+output = hh.output_info('Python-разработчик')
+print(*output, sep='\n')
