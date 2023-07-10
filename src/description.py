938aeba4b1024ecff97c450ec560566f558ccc34
Alexandra <alexandrasolovjova@mail.ru>
2023-07-10 18:23:05 +0300

    rename file, modify Description init

---
+++ src/description.py
@@ -0,0 +1,10 @@
+class Description:
+    def __init__(self, id, name, link, salary, description):
+        self.id = id
+        self.name = name
+        self.link = link
+        self.salary = salary
+        self.description = description
+        #self.working_condition = working_condition
+        #self.job_requirements = job_requirements
+
