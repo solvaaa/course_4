6a4644dcedacd8e9e9389c9e997fc2e31a346717
Alexandra <alexandrasolovjova@mail.ru>
2023-07-10 22:12:21 +0300

    added __lt__ method

--- src/description.py
+++ src/description.py
@@ -9,3 +9,17 @@ class Description:
     def __str__(self):
         return self.name
 
+    def __lt__(self, other):
+        if (self.salary is not None) and (self.salary['from'] is not None):
+            if (other.salary is not None) and (other.salary['from'] is not None):
+                return self.salary['from'] < other.salary['from']
+            else:
+                return False
+        elif (other.salary is not None) and (other.salary['from'] is not None):
+            return True
+        else:
+            return False
+
+    def __eq__(self, other):
+        if (self.salary is not None) and (other.salary is not None):
+            return self.salary['from'] == other.salary['from']
