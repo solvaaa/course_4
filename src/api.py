252c7b4e999f860ae45fdcf803bc23c2116337ee
Alexandra <alexandrasolovjova@mail.ru>
2023-07-11 21:00:20 +0300

    modify date_published format

--- src/api.py
+++ src/api.py
@@ -50,7 +50,7 @@ class HeadHunter(Api):
             date_raw = info['published_at']
             date_published = datetime.datetime.strptime(date_raw, '%Y-%m-%dT%H:%M:%S%z')
             date_published = datetime.datetime.replace(date_published, tzinfo=None)
-            date_published = date_published.strftime('Y-%m-%dT%H:%M:%S')
+            date_published = date_published.strftime('%Y-%m-%d %H:%M:%S')
             item = {
                 'id': id,
                 'name': name,
@@ -91,7 +91,7 @@ class SuperJob(Api):
 
             date_raw = info['date_published']
             date_published = datetime.datetime.fromtimestamp(date_raw)
-            date_published = date_published.strftime('Y-%m-%dT%H:%M:%S')
+            date_published = date_published.strftime('%Y-%m-%d %H:%M:%S')
 
             item = {
                 'id': id,
