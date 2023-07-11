4e47d66b1c3828ba7d7dd74fa0850ed93a299540
Alexandra <alexandrasolovjova@mail.ru>
2023-07-11 22:03:56 +0300

    add docstrings

--- src/saver.py
+++ src/saver.py
@@ -1,32 +1,56 @@
 import json
 from abc import ABC, abstractmethod
 from src.description import Description
-import datetime
 
 
 class Saver(ABC):
+    '''
+    Базовый класс для взаимодействия с файлами
+    '''
     @abstractmethod
     def read_datafile(self):
+        '''
+        Метод для чтения файла
+        '''
         pass
 
     @abstractmethod
     def add_descriptions(self, vacancies):
+        '''
+        Метод для добавления вакансий в файл
+        '''
         pass
 
     @abstractmethod
     def delete_description(self, vacancy):
+        '''
+        Метод для удаления вакансии из файла
+        '''
         pass
 
     @abstractmethod
     def get_by_keywords(self, keyword):
+        '''
+        Метод для получения вакансий из файла по ключевым словам
+        '''
         pass
 
 
 class JsonSaver(Saver):
+    '''
+    Класс для работы с файлами json
+    Опциональный параметр path - путь к файлу.
+    default path: descriptions.json
+    '''
     def __init__(self, path='descriptions.json'):
         self.path = path
 
     def read_datafile(self):
+        '''
+        Возвращает список словарей с данными о вакансии,
+        в формате класса Description
+        Если файл пустой или отсутствует - возвращает пустой списко
+        '''
         try:
             with open(self.path, 'r', encoding='utf-8') as file:
                 text = file.read()
@@ -38,6 +62,10 @@ class JsonSaver(Saver):
             return []
 
     def add_descriptions(self, vacancies):
+        '''
+        Добавляет вакансии
+        Принимает список экземпляров или экземпляр класса Description
+        '''
         descriptions = self.read_datafile()
         if isinstance(vacancies, Description):
             vacancies = [vacancies]
@@ -56,6 +84,10 @@ class JsonSaver(Saver):
             json.dump(descriptions, file)
 
     def delete_description(self, vacancy):
+        '''
+        Удаляет вакансию из файла.
+        Принимает на вход экземпляр класса Description
+        '''
         descriptions = self.read_datafile()
         for i in range(len(descriptions)):
             if descriptions[i]['id'] == vacancy.id:
@@ -66,6 +98,11 @@ class JsonSaver(Saver):
             json.dump(descriptions, file)
 
     def get_by_keywords(self, keywords):
+        '''
+        Возвращает список вакансий, содержащих ВСЕ ключевые слова
+        Принимает на вход строку ключевых слов, разделённых пробелами
+        Формат возврата: список словарей
+        '''
         descriptions = self.read_datafile()
         keywords = keywords.lower().split()
         filtered_descriptions = []
